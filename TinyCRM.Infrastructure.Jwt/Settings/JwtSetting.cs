535f4c6956207631647fb462aee3b3990f17401c
Ron Le <ronle9519@gmail.com>
2023-08-09 10:20:18 +0700

    chore: remove jwt project

--- TinyCRM.Infrastructure.Jwt/Settings/JwtSetting.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TinyCRM.Infrastructure.Jwt.Settings;
-
-public class JwtSetting
-{
-    public string Audience { get; set; }
-    public string Issuer { get; set; }
-    public string Key { get; set; }
-    public int Expires { get; set; }
-}
