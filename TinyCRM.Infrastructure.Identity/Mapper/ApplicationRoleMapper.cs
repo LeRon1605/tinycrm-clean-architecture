dde7cbeb21f493291e2f22f45e5ea5d0426b3832
Le Ron <78067510+LeRon1605@users.noreply.github.com>
2023-10-05 21:28:51 +0700

    fix: fix missed type mapping configuration

--- TinyCRM.Infrastructure.Identity/Mapper/ApplicationRoleMapper.cs
+++ TinyCRM.Infrastructure.Identity/Mapper/ApplicationRoleMapper.cs
@@ -7,6 +7,7 @@ public class ApplicationRoleMapper : IdentityMapper
     public ApplicationRoleMapper()
     {
         CreateMap<RoleCreateDto, ApplicationRole>();
+        CreateMap<RoleUpdateDto, ApplicationRole>();
         CreateMap<ApplicationRole, RoleDto>();
     }
 }
